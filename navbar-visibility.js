882082d623910739efa65fa5118e1f70b2651fcb
foxmaestro753 <fmypat@gmail.com>
2024-07-19 12:09:10 +0530

    Changed file structure

--- navbar-visibility.js
+++ navbar-visibility.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const navbar = document.querySelector('.navbar');
-  
-    window.addEventListener('scroll', function () {
-      if (window.scrollY > 100) {
-        navbar.classList.add('scrolled');
-      } else {
-        navbar.classList.remove('scrolled');
-      }
-    });
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 100) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
   });
-  
+});
